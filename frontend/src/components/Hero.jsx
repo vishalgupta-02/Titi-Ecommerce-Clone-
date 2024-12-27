f6c9c3929081a094757c2efa0985e1ffb8426cef
Vishal <abhimanyug987@gmail.com>
2024-12-27 16:46:25 +0000

    Collections page updated

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row border border-gray-400'>
-        {/* Hero lEft */}
+        {/* Hero lEft Side*/}
         <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
             <div className='text-[#414141]'>
                 <div className='flex items-center gap-2'>
